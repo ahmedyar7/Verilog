34f4daec7b880cd3e823b6654e2e715f8caffbff
ahmedyar7 <ahmedyar778@gmail.com>
2024-03-30 02:36:48 +0500

    Adder

--- Gate level modeling/halfaddar.v
+++ Gate level modeling/halfaddar.v
@@ -27,3 +27,38 @@ $monitor("Time = %t  A = %b  B = %b   C = %b  Y = %b",$time,A,B,C,Y);
 $finish;
 end 
 endmodule
+
+
+module logic(input A,B,C, output Q);
+    wire x,y,z;
+
+    nand(x,A,B);
+    or(y,A,B);
+    not(z,y);
+    and(Q,C,x,z);
+
+endmodule
+
+module testbench;
+
+reg A,B,C;
+wire Q;
+
+logic inst_logic(.A(A), .B(B), .C(C), .Q(Q));
+
+initial begin
+$monitor("Time = %t   A = %b   B = %b   C = %b   Q = %b",$time, A,B,C,Q);
+
+#5 A = 0; B = 0; C = 0;
+#5 A = 0; B = 0; C = 1;
+#5 A = 0; B = 1; C = 0;
+#5 A = 0; B = 1; C = 1;
+#5 A = 1; B = 0; C = 0;
+#5 A = 1; B = 0; C = 1;
+#5 A = 1; B = 1; C = 0;
+#5 A = 1; B = 1; C = 1;
+
+$finish;
+end 
+endmodule
+
