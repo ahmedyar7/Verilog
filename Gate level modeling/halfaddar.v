01eb6a4b1d5195bed5b099c2f2a404754f5bb6e0
ahmedyar7 <ahmedyar778@gmail.com>
2024-03-30 01:58:30 +0500

    Half Addar

--- Gate level modeling/halfaddar.v
+++ Gate level modeling/halfaddar.v
@@ -5,3 +5,25 @@ module half_adder(input A,B,C, output Y);
     or(b,a,D);
     not(Y,D);
 endmodule
+
+module testbench;
+reg A,B,C;
+wire Y;
+
+half_adder inst_half_adder(.A(A), .B(B), .C(C), .Y(Y));
+
+initial begin
+$monitor("Time = %t  A = %b  B = %b   C = %b  Y = %b",$time,A,B,C,Y);
+
+#5 A = 0; B = 0; C = 0;
+#5 A = 0; B = 0; C = 1;
+#5 A = 0; B = 1; C = 0;
+#5 A = 0; B = 1; C = 1;
+#5 A = 1; B = 0; C = 0;
+#5 A = 1; B = 0; C = 1;
+#5 A = 1; B = 1; C = 0;
+#5 A = 1; B = 1; C = 1;
+
+$finish;
+end 
+endmodule
