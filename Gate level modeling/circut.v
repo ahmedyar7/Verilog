bfc0a025ddeafc3f758ff7982ff4ffb1fcdf10ef
ahmedyar7 <ahmedyar778@gmail.com>
2024-04-03 23:51:54 +0500

    d

--- Gate level modeling/circut.v
+++ Gate level modeling/circut.v
@@ -1,11 +1,12 @@
-module circut(input A,B,C,D,E,F, output W);
-    wire G,H,I,J,K,L,M;
-    not(G,B);
-    and(H,A,G);
-    or(I,H,C);
-    not(J,D);
-    and(K,J,E);
-    not(L,F);
-    and(M,K,L);
-    and(W,I,M);
+module circuit(input A, B, C, D, E, F, output W);
+    wire G, H, I, J, K, L, M;
+    
+    not (G, B);
+    and (H, A, G);
+    or (I, H, C);
+    not (J, D);
+    and (K, J, E);
+    not (L, F);
+    and (M, K, L);
+    and (W, I, M);
 endmodule
