ad89cc77ab7247bc3959ba4954ab940570b9c6d7
Ahmed Yar <ahmedyar778@gmail.com>
2024-04-05 03:29:12 +0500

    Create mycircut.v

---
+++ Gate level modeling/mycircut.v
@@ -0,0 +1,13 @@
+module mycircut(input A, B, C, D, E, F, output M);
+    wire I, H, G, J, K, L;
+
+    not(G, C);
+    or(H, G, B);
+    and(I, A, H);
+
+    not(J, D);
+    xor(K, E, F);
+    and(L, J, K);
+    or(M, I, L);
+
+endmodule
